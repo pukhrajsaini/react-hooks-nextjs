e81af8cd791866d095b1d54e2c0aee033f2ce8b2
pukhraj saini <pukhraj.saini@appinventiv.com>
2024-01-12 12:10:14 +0530

    login module

---
+++ src/app/login/components/login-form.component.tsx
@@ -0,0 +1,5 @@
+import styles from "./login-form.module.css";
+
+const LoginForm = () => <div className={styles.container}></div>;
+
+export default LoginForm;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "./components/login-form.component";
+
+function Login() {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+}
+
+export default Login;
