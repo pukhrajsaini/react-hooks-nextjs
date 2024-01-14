9c66e3dcd7d796aefc46c99f4338510ce9eadece
pukhraj saini <pukhraj.saini@appinventiv.com>
2024-01-14 17:57:54 +0530

    useState hook form data

--- src/app/login/components/Login.tsx
+++ src/app/login/components/Login.tsx
@@ -1,118 +1,91 @@
-import { Formik, Form, Field } from "formik";
-import styles from "./Login.module.css";
-import * as yup from "yup";
+"use client";
+import { useState } from "react";
 
 export default function LoginForm() {
-  const loginSchema = yup.object().shape({
-    email: yup.string().email().min(2).required(),
-    password: yup.string().required().min(4),
+  const [formGroup, setFormGroup] = useState({
+    email: "",
+    password: "",
   });
+  const [text, setText] = useState("something");
+  const [count, setCount] = useState(0);
+  const [list, setList] = useState([]);
+
+  const handler = (event: any) => {
+    const { value, name } = event.target;
+    setFormGroup((prevData) => {
+      return { ...prevData, [name]: value };
+    });
+  };
 
   return (
     <>
-      <div className={styles.container}>
-        <div className="flex flex-col justify-around px-6 lg:px-8 bg-cyan-300	">
-          <div className="sm:mx-auto sm:w-full">
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Welcome to Admin Accelerator Panel
-            </h2>
-            <p className="text-center text-xl leading-9 tracking-tight text-gray-600">
-              Admin Accelerator offers the most convenient and reliable way to
-              get around town
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-col justify-center px-6 lg:px-8">
-          <div className="sm:mx-auto">
-            <img
-              className="mx-auto h-10 w-auto"
-              src="logo.svg"
-              alt="Your Company"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Welcome to Admin Accelerator Panel
-            </h2>
-          </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <h1>Email : {formGroup.email}</h1>
+        <h1>Password : {formGroup.password}</h1>
+      </div>
 
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <Formik
-              initialValues={{ email: "", password: "" }}
-              validationSchema={loginSchema}
-              onSubmit={(values) => {
-                console.log(values);
-              }}
-            >
-              {({
-                values,
-                errors,
-                touched,
-                isSubmitting,
-                handleBlur,
-                handleChange,
-                handleSubmit,
-                handleReset,
-              }) => (
-                <form className="space-y-6" onSubmit={handleSubmit}>
-                  <div>
-                    <label
-                      htmlFor="email"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      Email address
-                    </label>
-                    <div className="mt-2">
-                      <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        autoComplete="email"
-                        required
-                        className="block w-full  px-0.5 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      />
-                    </div>
-                  </div>
+      <div className="flex flex-col justify-center px-6 lg:px-8">
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  onChange={handler}
+                  className="block w-full  px-0.5 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-                  <div>
-                    <div className="flex items-center justify-between">
-                      <label
-                        htmlFor="password"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        Password
-                      </label>
-                      <div className="text-sm">
-                        <a
-                          href="#"
-                          className="font-semibold text-indigo-600 hover:text-indigo-500"
-                        >
-                          Forgot password?
-                        </a>
-                      </div>
-                    </div>
-                    <div className="mt-2">
-                      <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        autoComplete="current-password"
-                        required
-                        className="block  px-0.5 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      />
-                    </div>
-                  </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  onChange={handler}
+                  className="block  px-0.5 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-                  <div>
-                    <button
-                      type="submit"
-                      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                      Login
-                    </button>
-                  </div>
-                </form>
-              )}
-            </Formik>
-          </div>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Login
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </>
